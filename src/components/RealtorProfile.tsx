789009866e9259d7459268d40505176ea521f6af
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2022-09-13 17:26:53 -0300

    Add RealtorProfile component and use as list header for the flatlist of posts in profile screen

---
+++ src/components/RealtorProfile.tsx
@@ -0,0 +1,144 @@
+import { View, StyleSheet, Text, Image, TouchableOpacity } from 'react-native'
+// import { MaterialIcons } from '@expo/vector-icons'
+import { useTheme } from '@react-navigation/native'
+import { UserInfo } from '../screens/Profile'
+
+type Props = {
+  userInfo: UserInfo,
+}
+export const RealtorProfile: React.FC<Props> = ({ userInfo }) => {
+  const { colors } = useTheme()
+
+  const styles = StyleSheet.create({
+    container: {
+      width: '100%',
+    },
+    title: {
+      fontSize: 24,
+      color: colors.text,
+    },
+    headerContainer: {
+      flexDirection: 'row',
+      height: 50,
+      borderBottomWidth: 1,
+      borderStyle: 'solid',
+      borderBottomColor: colors.text,
+      width: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    profilePicture: {
+      width: 100,
+      height: 100,
+      borderRadius: 50,
+    },
+    userInfoContainer: {
+      alignItems: 'center',
+      paddingTop: 10,
+    },
+    userInfoHeader: {
+      flexDirection: 'row',
+      paddingTop: 10,
+    },
+    leftContainer: {
+      height: 160,
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      paddingHorizontal: 10,
+    },
+    msgButton: {
+      backgroundColor: colors.primary,
+      paddingHorizontal: 8,
+      paddingVertical: 4,
+      borderRadius: 3,
+    },
+    msgButtonText: {
+      fontSize: 18,
+      color: colors.text,
+    },
+    descriptionContainer: {
+      width: '60%',
+      paddingLeft: 10,
+    },
+    description: {
+      fontSize: 15,
+      color: colors.text,
+      textAlign: 'justify',
+    },
+    statisticsContainer: {
+      width: '80%',
+      paddingVertical: 10,
+      height: 120,
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+    },
+    dummyStats: {
+      backgroundColor: colors.primary,
+      height: 80,
+      width: 80,
+      borderRadius: 40,
+    },
+  })
+
+  return (
+    <View>
+      <View style={styles.headerContainer}>
+        {/* <TouchableHighlight>
+          <Text>Voltar</Text>
+        </TouchableHighlight> */}
+        <Text style={styles.title}>{userInfo.name}</Text>
+      </View>
+      <View style={styles.userInfoContainer}>
+        <View style={styles.userInfoHeader}>
+          <View style={styles.leftContainer}>
+            <Image
+              style={styles.profilePicture}
+              source={{ uri: userInfo.photo }}
+            />
+            <TouchableOpacity
+              style={styles.msgButton}
+              onPress={() => console.warn('Abrir whatsapp ' + userInfo.phone)}>
+              <Text style={styles.msgButtonText}>Mensagem</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.descriptionContainer}>
+            <Text style={styles.description}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente
+              consequuntur expedita vero, tenetur pariatur aliquam, obcaecati
+              error quia earum ipsam debitis doloremque laboriosam aut aliquid
+              ab quasi blanditiis asperiores modi?
+            </Text>
+          </View>
+        </View>
+        <View>
+          <Text style={{ color: colors.text, fontSize: 20 }}>Estatísticas</Text>
+          <View style={styles.statisticsContainer}>
+            <View style={{ alignItems: 'center' }}>
+              <View style={styles.dummyStats} />
+              <Text style={{ color: colors.text }}>Vendas</Text>
+            </View>
+            <View style={{ alignItems: 'center' }}>
+              <View style={styles.dummyStats} />
+              <Text style={{ color: colors.text }}>Alugueis</Text>
+            </View>
+            <View style={{ alignItems: 'center' }}>
+              <View style={styles.dummyStats} />
+              <Text style={{ color: colors.text }}>Avaliações</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 40,
+    backgroundColor: '#EEE',
+    borderRadius: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+})
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,18 +1,10 @@
-import {
-  FlatList,
-  Image,
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
-} from 'react-native'
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
 import { BottomTabParamList, StackParamList } from '../Navigator'
 import { useTheme } from '@react-navigation/native'
 import type { CompositeScreenProps } from '@react-navigation/native'
 import type { BottomTabScreenProps } from '@react-navigation/bottom-tabs'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RealtorProfile } from '../components/RealtorProfile'
 
 type ProfileScreenNavigationProp = CompositeScreenProps<
   BottomTabScreenProps<BottomTabParamList, 'Profile'>,
@@ -20,7 +12,21 @@ type ProfileScreenNavigationProp = CompositeScreenProps<
 >
 // type Props = BottomTabScreenProps<BottomTabParamList, 'Profile'>
 
-const userInfo = {
+export interface UserInfo {
+  id: number;
+  email: string;
+  photo: string;
+  name: string;
+  isRealtor: true;
+  creci: string;
+  stars: number;
+  comletedSells: number;
+  completedRents: number;
+  phone: string;
+  posts: any[];
+}
+
+const userInfo: UserInfo = {
   id: 11,
   email: 'gustavo@gmail.com',
   photo: 'https://picsum.photos/seed/picsum/300',
@@ -44,7 +50,7 @@ const userInfo = {
       images: [
         {
           id: 2,
-          link: 'https://picsum.photos/seed/picsum/200',
+          link: 'https://picsum.photos/id/231/400',
         },
       ],
     },
@@ -60,7 +66,7 @@ const userInfo = {
       images: [
         {
           id: 3,
-          link: 'https://picsum.photos/seed/picsum/300',
+          link: 'https://picsum.photos/id/22/400',
         },
       ],
     },
@@ -76,7 +82,39 @@ const userInfo = {
       images: [
         {
           id: 4,
-          link: 'https://picsum.photos/seed/picsum/400',
+          link: 'https://picsum.photos/id/222/400',
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: 'Apartamento',
+      description: 'Bela apartamento com dois quartos',
+      totalArea: 310,
+      usefulArea: 300,
+      bathrooms: 2,
+      bedrooms: 2,
+      suites: 1,
+      images: [
+        {
+          id: 5,
+          link: 'https://picsum.photos/id/234/400',
+        },
+      ],
+    },
+    {
+      id: 5,
+      title: 'Apartamento',
+      description: 'Bela apartamento com dois quartos',
+      totalArea: 310,
+      usefulArea: 300,
+      bathrooms: 2,
+      bedrooms: 2,
+      suites: 1,
+      images: [
+        {
+          id: 6,
+          link: 'https://picsum.photos/id/237/400',
         },
       ],
     },
@@ -92,92 +130,28 @@ export const Profile: React.FC<ProfileScreenNavigationProp> = ({
   const styles = StyleSheet.create({
     container: {
       width: '100%',
-      flexGrow: 1,
-      backgroundColor: colors.background,
       alignItems: 'center',
-      justifyContent: 'flex-start',
     },
     title: {
       fontSize: 24,
       color: colors.text,
     },
-    headerContainer: {
-      flexDirection: 'row',
-      height: 50,
-      borderBottomWidth: 1,
-      borderStyle: 'solid',
-      borderBottomColor: colors.text,
-      width: '100%',
+    postsList: {
+      // justifyContent: 'space-around',
+      width: '111%',
+
       alignItems: 'center',
-      justifyContent: 'center',
-    },
-    profilePicture: {
-      width: 100,
-      height: 100,
-      borderRadius: 50,
-    },
-    userInfoContainer: {
-      alignItems: 'center',
-      paddingTop: 10,
-    },
-    userInfoHeader: {
-      flexDirection: 'row',
-      paddingTop: 10,
-    },
-    leftContainer: {
-      height: 160,
-      justifyContent: 'space-around',
-      alignItems: 'center',
-      paddingHorizontal: 10,
-    },
-    msgButton: {
-      backgroundColor: colors.primary,
-      paddingHorizontal: 8,
-      paddingVertical: 4,
-      borderRadius: 3,
-    },
-    msgButtonText: {
-      fontSize: 18,
-      color: colors.text,
-    },
-    descriptionContainer: {
-      width: '60%',
-      paddingLeft: 10,
-    },
-    description: {
-      fontSize: 15,
-      color: colors.text,
-      textAlign: 'justify',
-    },
-    statisticsContainer: {
-      width: '80%',
-      paddingVertical: 10,
-      height: 120,
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-    },
-    dummyStats: {
-      backgroundColor: colors.primary,
-      height: 80,
-      width: 80,
-      borderRadius: 40,
-    },
-    postsContainer: {
-      width: '100%',
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      flexGrow: 1,
     },
     post: {
       alignItems: 'center',
+      padding: 15,
     },
     postText: {
       fontSize: 18,
       color: colors.text,
     },
     dummyPostImage: {
-      backgroundColor: colors.primary,
+      // backgroundColor: colors.primary,
       height: 150,
       width: 150,
       borderRadius: 20,
@@ -185,78 +159,24 @@ export const Profile: React.FC<ProfileScreenNavigationProp> = ({
   })
 
   return (
-    <View style={{ width: '100%', marginBottom: 50 }}>
-      <View style={styles.headerContainer}>
-        {/* <TouchableHighlight>
-          <Text>Voltar</Text>
-        </TouchableHighlight> */}
-        <Text style={styles.title}>{userInfo.name}</Text>
-      </View>
-      <ScrollView
-        contentContainerStyle={{
-          alignItems: 'center',
-          justifyContent: 'flex-start',
-          width: '100%',
-        }}>
-        <View style={styles.userInfoContainer}>
-          <View style={styles.userInfoHeader}>
-            <View style={styles.leftContainer}>
-              <Image
-                style={styles.profilePicture}
-                source={{ uri: userInfo.photo }}
-              />
-              <TouchableHighlight style={styles.msgButton}>
-                <Text style={styles.msgButtonText}>Mensagem</Text>
-              </TouchableHighlight>
-            </View>
-            <View style={styles.descriptionContainer}>
-              <Text style={styles.description}>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Sapiente consequuntur expedita vero, tenetur pariatur aliquam,
-                obcaecati error quia earum ipsam debitis doloremque laboriosam
-                aut aliquid ab quasi blanditiis asperiores modi?
-              </Text>
-            </View>
-          </View>
-          <View>
-            <Text style={{ color: colors.text, fontSize: 20 }}>
-              Estatísticas
-            </Text>
-            <View style={styles.statisticsContainer}>
-              <View style={{ alignItems: 'center' }}>
-                <View style={styles.dummyStats} />
-                <Text style={{ color: colors.text }}>Vendas</Text>
-              </View>
-              <View style={{ alignItems: 'center' }}>
-                <View style={styles.dummyStats} />
-                <Text style={{ color: colors.text }}>Alugueis</Text>
-              </View>
-              <View style={{ alignItems: 'center' }}>
-                <View style={styles.dummyStats} />
-                <Text style={{ color: colors.text }}>Avaliações</Text>
-              </View>
-            </View>
-          </View>
-        </View>
-        <View style={styles.postsContainer}>
+    <View style={styles.container}>
+      <FlatList
+        data={userInfo.posts}
+        keyExtractor={post => `${post.id}`}
+        numColumns={2}
+        ListHeaderComponent={() => <RealtorProfile userInfo={userInfo} />}
+        contentContainerStyle={styles.postsList}
+        renderItem={({ item }) => (
           <View style={styles.post}>
-            <View style={styles.dummyPostImage} />
-            <Text style={styles.postText}>Post 1</Text>
+            {/* <View style={styles.dummyPostImage} /> */}
+            <Image
+              style={styles.dummyPostImage}
+              source={{ uri: item.images[0].link }}
+            />
+            <Text style={styles.postText}>{item.title}</Text>
           </View>
-          <View style={styles.post}>
-            <View style={styles.dummyPostImage} />
-            <Text style={styles.postText}>Post 2</Text>
-          </View>
-          <View style={styles.post}>
-            <View style={styles.dummyPostImage} />
-            <Text style={styles.postText}>Post 1</Text>
-          </View>
-          <View style={styles.post}>
-            <View style={styles.dummyPostImage} />
-            <Text style={styles.postText}>Post 2</Text>
-          </View>
-        </View>
-      </ScrollView>
+        )}
+      />
     </View>
   )
 }
