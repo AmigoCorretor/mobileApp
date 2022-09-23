474ecb72cddb72de896a8125f3fc2adadb7e5965
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2022-09-23 11:37:34 -0300

    Add dismiss keyboard when clicking on backgorund. Add KeyboardAvoidingView

--- src/screens/Publication.tsx
+++ src/screens/Publication.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect } from "react"
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs"
 import { CompositeScreenProps, useTheme } from "@react-navigation/native"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
-import { SafeAreaView, TouchableOpacity, View, Text, StyleSheet } from "react-native"
+import { SafeAreaView, TouchableOpacity, View, Text, StyleSheet, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, ScrollView } from "react-native"
 import { MaterialIcons } from '@expo/vector-icons'
 
 import { AuthInput } from '../components/AuthInput'
@@ -51,10 +51,15 @@ export const Publication: React.FC<PublicationScreenNavigationProp> = () => {
             width: '90%',
             alignItems: 'center'
         },
-        infosInputData: {
+        adicitionalInfosInputData: {
             color: colors.text,
             fontSize: 17,
             textAlign: 'center'
+        },
+        inputs: {
+            marginVertical: 10,
+            borderRadius: 5,
+            width: '90%'
         },
         numberInputData: {
             width: 150,
@@ -66,11 +71,6 @@ export const Publication: React.FC<PublicationScreenNavigationProp> = () => {
             justifyContent: 'space-around',
             width: '80%',
             marginTop: 10
-        },
-        inputs: {
-            marginVertical: 10,
-            borderRadius: 5,
-            width: '90%'
         },
         button: {
             width: 100,
@@ -130,102 +130,107 @@ export const Publication: React.FC<PublicationScreenNavigationProp> = () => {
     }
 
     return (
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>Nova Publicação</Text>
-            <AuthInput
-                icon='title'
-                style={styles.inputs}
-                placeholder='Título'
-                value={title}
-                onChangeText={setTitle}
-                placeholderTextColor='#333'
-            />
-            <AuthInput
-                icon='photo'
-                style={styles.inputs}
-                placeholder='Foto'
-                value={image}
-                onChangeText={setImage}
-                placeholderTextColor='#333'
-            />
-            <AuthInput
-                icon='description'
-                style={styles.inputs}
-                placeholder='Descrição'
-                value={description}
-                onChangeText={setDescription}
-                placeholderTextColor='#333'
-            />
-
-            <View style={styles.viewNumberInputData}>
-
-                <Text style={[styles.numberInputData, styles.infosInputData]}>Informações Adicionais:</Text>
-
-                <AuthInput
-                    icon='crop-din'
-                    style={styles.numberInputData}
-                    placeholder='Área total'
-                    value={totalArea}
-                    onChangeText={setTotalArea}
-                    placeholderTextColor='#333'
-                />
-                <AuthInput
-                    icon='aspect-ratio'
-                    style={styles.numberInputData}
-                    placeholder='Área útil'
-                    value={usefulArea}
-                    onChangeText={setUseFulArea}
-                    placeholderTextColor='#333'
-                />
-                <AuthInput
-                    icon='bathtub'
-                    style={styles.numberInputData}
-                    placeholder='Banheiro(s)'
-                    value={bathrooms}
-                    onChangeText={setBathrooms}
-                    placeholderTextColor='#333'
-                />
-                <AuthInput
-                    icon='airline-seat-individual-suite'
-                    style={styles.numberInputData}
-                    placeholder='Quarto(s)'
-                    value={bedrooms}
-                    onChangeText={setBedrooms}
-                    placeholderTextColor='#333'
-                />
-                <AuthInput
-                    icon='king-bed'
-                    style={styles.numberInputData}
-                    placeholder='Suítes'
-                    value={suites}
-                    onChangeText={setSuites}
-                    placeholderTextColor='#333'
-                />
-            </View>
-            <View style={styles.viewButton}>
-                <TouchableOpacity
-                    style={[styles.button, { backgroundColor: '#F88' }]}
-                    onPress={() => { }}>
-                    <MaterialIcons
-                        name='cancel'
-                        size={20}
-                        style={styles.cancelButtonIcon}
+        <SafeAreaView>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            >
+                <ScrollView keyboardShouldPersistTaps='handled'
+                    contentContainerStyle={styles.container}
+                >
+                    <Text style={styles.title}>Nova Publicação</Text>
+                    <AuthInput
+                        icon='title'
+                        style={styles.inputs}
+                        placeholder='Título'
+                        value={title}
+                        onChangeText={setTitle}
+                        placeholderTextColor='#333'
                     />
-                    <Text>Cancelar</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={[styles.button, validPost ? { backgroundColor: '#8F8' } : { backgroundColor: '#AAA' }]}
-                    onPress={saveNewPost}
-                    disabled={!validPost}>
-                    <MaterialIcons
-                        name='save'
-                        size={20}
-                        style={styles.cancelButtonIcon}
+                    <AuthInput
+                        icon='photo'
+                        style={styles.inputs}
+                        placeholder='Foto'
+                        value={image}
+                        onChangeText={setImage}
+                        placeholderTextColor='#333'
                     />
-                    <Text>Salvar</Text>
-                </TouchableOpacity>
+                    <AuthInput
+                        icon='description'
+                        style={styles.inputs}
+                        placeholder='Descrição'
+                        value={description}
+                        onChangeText={setDescription}
+                        placeholderTextColor='#333'
+                    />
 
-            </View>
+                    <View style={styles.viewNumberInputData}>
+                        <Text style={[styles.numberInputData, styles.adicitionalInfosInputData]}>Informações Adicionais:</Text>
+                        <AuthInput
+                            icon='crop-din'
+                            style={styles.numberInputData}
+                            placeholder='Área total'
+                            value={totalArea}
+                            onChangeText={setTotalArea}
+                            placeholderTextColor='#333'
+                        />
+                        <AuthInput
+                            icon='aspect-ratio'
+                            style={styles.numberInputData}
+                            placeholder='Área útil'
+                            value={usefulArea}
+                            onChangeText={setUseFulArea}
+                            placeholderTextColor='#333'
+                        />
+                        <AuthInput
+                            icon='bathtub'
+                            style={styles.numberInputData}
+                            placeholder='Banheiro(s)'
+                            value={bathrooms}
+                            onChangeText={setBathrooms}
+                            placeholderTextColor='#333'
+                        />
+                        <AuthInput
+                            icon='airline-seat-individual-suite'
+                            style={styles.numberInputData}
+                            placeholder='Quarto(s)'
+                            value={bedrooms}
+                            onChangeText={setBedrooms}
+                            placeholderTextColor='#333'
+                        />
+                        <AuthInput
+                            icon='king-bed'
+                            style={styles.numberInputData}
+                            placeholder='Suítes'
+                            value={suites}
+                            onChangeText={setSuites}
+                            placeholderTextColor='#333'
+                        />
+                    </View>
+                    <View style={styles.viewButton}>
+                        <TouchableOpacity
+                            style={[styles.button, { backgroundColor: '#F88' }]}
+                            onPress={() => { }}>
+                            <MaterialIcons
+                                name='cancel'
+                                size={20}
+                                style={styles.cancelButtonIcon}
+                            />
+                            <Text>Cancelar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={[styles.button, validPost ? { backgroundColor: '#8F8' } : { backgroundColor: '#AAA' }]}
+                            onPress={saveNewPost}
+                            disabled={!validPost}>
+                            <MaterialIcons
+                                name='save'
+                                size={20}
+                                style={styles.cancelButtonIcon}
+                            />
+                            <Text>Salvar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
+            </KeyboardAvoidingView>
         </SafeAreaView>
     )
 }
